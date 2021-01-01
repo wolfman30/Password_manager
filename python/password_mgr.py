b35c1eb0f8403c33fd8bfd9e15ad234f6fcc2990
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-01 00:22:11 -0500

    Added letters and numbers use for security levels.

--- python/password_mgr.py
+++ python/password_mgr.py
@@ -1,3 +1,10 @@
+import string 
+
+letters = string.ascii_letters #reters to all English letters in lowercase and upper case in a string
+numbers = list(range(0, 10)) #refers to all numbers from 0 to 9 in a list
+print(letters)
+print(numbers)
+
 class BasePasswordManager:
 
     #list holding all user's past passwords
