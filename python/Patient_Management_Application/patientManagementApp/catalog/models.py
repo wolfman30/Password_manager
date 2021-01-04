b012094007290f24e1a98dcaaf2eb20dd7f36115
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-04 12:51:14 -0500

    Added fields of data to input for each patient.

--- python/Patient_Management_Application/patientManagementApp/catalog/models.py
+++ python/Patient_Management_Application/patientManagementApp/catalog/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Patient(models.Model): 
-    pass
+    uniqueID = models.CharField(max_length=20, help_text="Enter the patient's unique ID: ")
+    firstName = models.CharField(max_length=20, help_text="Enter the patient's unique ID: ")
+    lastName = models.CharField(max_length=20, help_text="Enter the patient's unique ID: ")
+    dob = models.CharField(max_length=20, help_text="Enter the patient's unique ID: ")
+
+    class Meta: 
+        ordering = ['-uniqueID', '-firstName', '-lastName', '-dob']
+
+    def get_absolute_url(self): 
+        """Returns the url to access a particular instance of Patient"""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self): 
+        return (self.uniqueID, self.firstName, self.lastName, self.dob)
